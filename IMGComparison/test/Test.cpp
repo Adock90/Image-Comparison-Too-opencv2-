a7881105c412b76f55486073ab22aea71520c131
Adock90 <croft5571@gmail.com>
2024-10-19 16:48:29 +0100

    Update Test.cpp

--- IMGComparison/test/Test.cpp
+++ IMGComparison/test/Test.cpp
@@ -1,12 +1,17 @@
+//includes header
 #include "IMGComparsion.hpp"
+// for messagebox
 #include <windows.h>
 
 using namespace IMGComparison;
 
 int main()
 {
+    //creates onject
     IMG Image;
+    //uses function
     bool Check = IsImageTheSame("hi.png", "hi2.png");
+    // checks result and outputs mesagebox
     switch (Check)
     {
     case true:
